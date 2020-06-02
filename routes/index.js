3622bfd63fbf568adcda953a1f8dd41be37146b1
romma1620 <romankuchera@gmail.com>
2020-06-02 18:42:08 +0300

    dz v.0.1

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+module.exports.userRouter = require('./user/user.router')
+
+
+// const userRouter = require('./user/user.router')
+// const productRouter = require('./product/product.router')
+//
+// module.exports = {
+//     userRouter,
+//     productRouter
+// }
---
+++ service/index.js
@@ -0,0 +1,2 @@
+// module.exports.userService = require('./user.service.old');
+module.exports.userService = require('./user.service')
